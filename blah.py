987f5be893f2331a178e4b64a0c354190376715e
Ling <lingd1944@gmail.com>
2015-05-08 12:30:24 -0400

    register

--- blah.py
+++
@@ -1,4 +0,0 @@
-import sqlite3
-from utils import manager
-
-manager.addEvent("2015-05-06 12:29:36","Testing","TLiang518","This should be a working event","Stuyvesant High School", "7th Period", "#yoloswag #peep")
